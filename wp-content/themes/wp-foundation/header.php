7115c3d6d7c2f0696dc88af7e6672f18f536330f
Marshall Bauernfeind <marshall@inversoft.com>
2014-03-11 18:35:54 -0600

    From office

--- wp-content/themes/wp-foundation/header.php
+++ wp-content/themes/wp-foundation/header.php
@@ -44,77 +44,11 @@
 
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 
-		
+
 		<!-- bring in theme options styles -->
-		<?php 
-		$heading_typography = of_get_option('heading_typography');
-		if ($heading_typography) {
-			$theme_options_styles = '
-			h1, h2, h3, h4, h5, h6{ 
-				font-family: ' . $heading_typography['face'] . '; 
-				font-weight: ' . $heading_typography['style'] . '; 
-				color: ' . $heading_typography['color'] . '; 
-			}';
-		}
-		
-		$main_body_typography = of_get_option('main_body_typography');
-		if ($main_body_typography) {
-			$theme_options_styles .= '
-			body{ 
-				font-family: ' . $main_body_typography['face'] . '; 
-				font-weight: ' . $main_body_typography['style'] . '; 
-				color: ' . $main_body_typography['color'] . '; 
-			}';
-		}
-		
-		$link_color = of_get_option('link_color');
-		if ($link_color) {
-			$theme_options_styles .= '
-			a{ 
-				color: ' . $link_color . '; 
-			}';
-		}
-		
-		$link_hover_color = of_get_option('link_hover_color');
-		if ($link_hover_color) {
-			$theme_options_styles .= '
-			a:hover{ 
-				color: ' . $link_hover_color . '; 
-			}';
-		}
-		
-		$link_active_color = of_get_option('link_active_color');
-		if ($link_active_color) {
-			$theme_options_styles .= '
-			a:active{ 
-				color: ' . $link_active_color . '; 
-			}';
-		}
-		
-		$topbar_bg_color = of_get_option('top_nav_bg_color');
-		if ($topbar_bg_color) {
-			$theme_options_styles .= '
-			.top-nav { 
-				background-color: '. $topbar_bg_color . ';
-			}';
-		}
-		
-		$topbar_link_color = of_get_option('top_nav_link_color');
-		if ($topbar_link_color) {
-			$theme_options_styles .= '
-			.top-nav > li > a { 
-				color: '. $topbar_link_color . ' !important;
-			}';
-		}
-		
-		$topbar_link_hover_color = of_get_option('top_nav_link_hover_color');
-		if ($topbar_link_hover_color) {
-			$theme_options_styles .= '
-			.top-nav > li > a:hover { 
-				color: '. $topbar_link_hover_color . ' !important;
-			}';
-		}
-		
+		<?php
+
+
 		$suppress_comments_message = of_get_option('suppress_comments_message');
 		if ($suppress_comments_message){
 			$theme_options_styles .= '
@@ -122,9 +56,9 @@
 				border-bottom: none;
 			}';
 		}
-						
+
 		if($theme_options_styles){
-			echo '<style>' 
+			echo '<style>'
 			. $theme_options_styles . '
 			</style>';
 		}
@@ -165,6 +99,7 @@
 
 				</header> <!-- end header -->
       </div>
+      <hr  />
       <div class="twelve columns main-nav">
 
         <?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
--- wp-content/themes/wp-foundation/page-full-width.php
+++ wp-content/themes/wp-foundation/page-full-width.php
@@ -12,14 +12,7 @@ Template Name: Full Width Page
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-						
-						<header>
-							
-							<h1><?php the_title(); ?></h1>
-						
-						</header> <!-- end article header -->
-					
+
 						<section class="post_content">
 							<?php the_content(); ?>
 					
@@ -31,24 +24,10 @@ Template Name: Full Width Page
 							
 						</footer> <!-- end article footer -->
 					
-					</article> <!-- end article -->
-					
-					<?php comments_template(); ?>
-					
-					<?php endwhile; ?>	
+
+					<?php endwhile; ?>
 					
 					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1>Not Found</h1>
-					    </header>
-					    <section class="post_content">
-					    	<p>Sorry, but the requested resource was not found on this site.</p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
 					
 					<?php endif; ?>
 			
